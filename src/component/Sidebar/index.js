571428af10c8b6a2253a8929d19b2262013916fb
Thaiebu <thaiebu785@gmail.com>
2022-11-17 15:01:36 +0530

    mobile view updated

--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -1,51 +1,107 @@
-import {Link, NavLink} from 'react-router-dom';
 import './index.scss';
+import { useState } from 'react';
 import LogoS from '../../assets/images/Miracle.jpg';
-//import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome,faEnvelope,faUser} from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF,faLinkedin,faGithub } from '@fortawesome/free-brands-svg-icons'
-const Sidebar = () => (
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLinkedin,
+  faGithub,
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link, NavLink } from 'react-router-dom';
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
+
+    return(
+
     <div className='nav-bar'>
-        <Link className='logo' to='/portfolio/'>
-            <img src={LogoS} alt='s-log'/>
-            <p class="name">Mohamed</p>
-        </Link>
+    <Link className='logo' to='/portfolio/' onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt='s-log'/>
+        <p class="name">Mohamed</p>
+    </Link>
 
-        <nav>
-            <NavLink exact="true" activeclassnam="active" to="/portfolio/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
-  
+    {/* <nav>
+        <NavLink exact="true" activeclassnam="active" to="/portfolio/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+        </NavLink>
 
-            <NavLink exact="true" activeclassnam="active" className="about-link" to="/portfolio/about">
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
 
-            <NavLink exact="true" activeclassnam="active" className="contact-link" to="/portfolio/contact">
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
-        </nav>
+        <NavLink exact="true" activeclassnam="active" className="about-link" to="/portfolio/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+        </NavLink>
 
-        <ul>
-            <li>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mohamed-thaiebu-0015a815a">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noopener noreferrer" href="/">
-                    <FontAwesomeIcon icon={faFacebookF} color="#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/Thaiebu/">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
-                </a>
-            </li>
-        </ul>
+        <NavLink exact="true" activeclassnam="active" className="contact-link" to="/portfolio/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+        </NavLink>
+    </nav> */}
+    <nav className={showNav ? 'mobile-show' : ''}>
+    <NavLink 
+      exact="true"
+      activeclassname="active"
+      to="/portfolio/"
+      onClick={() => setShowNav(false)}>
+      <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+    </NavLink>
+    <NavLink 
+      activeclassname="active"
+      className="about-link"
+      to="/portfolio/about"
+      onClick={() => setShowNav(false)}>
+      <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+    </NavLink>
+    <NavLink
+      activeclassname="active"
+      className="contact-link"
+      to="/portfolio/contact"
+      onClick={() => setShowNav(false)}
+    >
+      <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+    </NavLink>
+    <FontAwesomeIcon 
+      onClick={() => setShowNav(false)}
+      icon={faClose}
+      color="#ffd700"
+      size="3x"
+      className='close-icon' />
+  </nav>
 
-    </div>
-)
+    <ul>
+        <li>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mohamed-thaiebu-0015a815a">
+                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+            </a>
+        </li>
+        <li>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Thaiebu/">
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+            </a>
+        </li>
+        <li>
+            <a target="_blank" rel="noopener noreferrer" href="/">
+                <FontAwesomeIcon icon={faFacebook} color="#4d4d4e"/>
+            </a>
+        </li>
+
+    </ul>
+    <FontAwesomeIcon 
+      onClick={() => setShowNav(true)}
+      icon={faBars}
+      color="#ffd700"
+      size="3x"
+      className='hamburger-icon' />
+
+</div>
+
+    )
+
+}
 
 export default Sidebar
+
+
