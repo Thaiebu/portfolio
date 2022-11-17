73c4e3ef1319b6f508ff2ab6c7fe696286a7365e
Thaiebu <thaiebu785@gmail.com>
2022-11-17 12:19:07 +0530

    some changes

--- src/component/Sidebar/index.js
+++ src/component/Sidebar/index.js
@@ -1,19 +1,19 @@
 import {Link, NavLink} from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/Miracle.jpg';
+//import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome,faEnvelope,faUser} from "@fortawesome/free-solid-svg-icons";
 import { faFacebookF,faLinkedin,faGithub } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
+        <Link className='logo' to='/portfolio/'>
             <img src={LogoS} alt='s-log'/>
-            <img className='sub-logo' src={LogoSubtitle} alt='s-LogoSubtitle'/>
+            <p>Mohamed</p>
         </Link>
 
         <nav>
-            <NavLink exact="true" activeclassnam="active" to="/">
+            <NavLink exact="true" activeclassnam="active" to="/portfolio/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
             </NavLink>
   
